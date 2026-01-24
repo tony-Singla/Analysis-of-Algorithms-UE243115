a5ca71742b53de2d0c25656788cd55565391a661
tony-Singla <tonysingla19@gmail.com>
2026-01-24 12:58:31 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sum {
+
+    static int sum1(int i ){
+        if(i==0) return 0 ;
+        return i + sum1(i-1); 
+    }
+    public static void main(String[] args) {
+        Scanner sc = new  Scanner(System.in);
+        int sum = sc.nextInt();
+        System.out.println(sum1(sum));
+        sc.close();
+    }
+}
