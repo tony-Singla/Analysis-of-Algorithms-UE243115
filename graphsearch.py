a414b964f8bd31f146e362210a6b45b2a837d713
tony-Singla <tonysingla19@gmail.com>
2026-02-01 13:29:02 +0530

    Add files via upload

---
+++ graphsearch.py
@@ -0,0 +1,31 @@
+import time
+import matplotlib.pyplot as plt
+import sys
+
+sys.setrecursionlimit(20000)
+
+def linear_search(arr, k, i):
+    if i == len(arr):
+        return False
+    if arr[i] == k:
+        return True
+    return linear_search(arr, k, i + 1)
+
+n_values = [100, 500, 1000, 2000, 4000, 8000]
+time_taken = []
+
+for n in n_values:
+    arr = list(range(n))
+    k = -1   
+
+    start = time.time()
+    linear_search(arr, k, 0)
+    end = time.time()
+
+    time_taken.append(end - start)
+
+plt.plot(n_values, time_taken, marker='o')
+plt.xlabel("n (array size)")
+plt.ylabel("Time")
+plt.title("n vs Time for Recursive Linear Search (O(n))")
+plt.show()
